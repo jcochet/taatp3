6cf0bfff8dd2af4605c6e6b64e4a937fabf07740
jcochet <juliencochet35@gmail.com>
2021-09-20 10:51:47 +0200

    Correction workercontroller

--- src/main/java/sample/data/jpa/web/WorkerController.java
+++ src/main/java/sample/data/jpa/web/WorkerController.java
@@ -1,5 +1,7 @@
 package sample.data.jpa.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -7,123 +9,117 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import sample.data.jpa.domain.Appointment;
-import sample.data.jpa.domain.User;
 import sample.data.jpa.domain.Worker;
-import sample.data.jpa.service.UserDao;
 import sample.data.jpa.service.WorkerDao;
 
-import java.util.List;
-
 @Controller
-public class WorkerController  {
+public class WorkerController {
 
-    /**
-     * GET /create  --> Create a new user and save it in the database.
-     */
-    @RequestMapping("/create")
-    @ResponseBody
-    public String create(String email, String name, String password, String job, List<Appointment> ap) {
-        String userId = "";
-        try {
-            Worker w = new Worker(email, name, password,  job,  ap);
-            workerDao.save(w);
-            userId = String.valueOf(w.getId());
-        }
-        catch (Exception ex) {
-            return "Error creating the user: " + ex.toString();
-        }
-        return "User succesfully created with id = " + userId;
-    }
+	/**
+	 * GET /create --> Create a new worker and save it in the database.
+	 */
+	@RequestMapping("/create")
+	@ResponseBody
+	public String create(String email, String name, String password, String job) {
+		String workerId = "";
+		try {
+			Worker worker = new Worker(email, name, password, job);
+			workerDao.save(worker);
+			workerId = String.valueOf(worker.getId());
+		} catch (Exception ex) {
+			return "Error creating the worker: " + ex.toString();
+		}
+		return "Worker succesfully created with id = " + workerId;
+	}
 
-    /**
-     * GET /delete  --> Delete the user having the passed id.
-     */
-    @RequestMapping("/delete")
-    @ResponseBody
-    public String delete(long id) {
-        try {
-            Worker worker = new Worker(id);
-            workerDao.delete(worker);
-        }
-        catch (Exception ex) {
-            return "Error deleting the user:" + ex.toString();
-        }
-        return "User succesfully deleted!";
-    }
+	/**
+	 * GET /delete --> Delete the worker having the passed id.
+	 */
+	@RequestMapping("/delete")
+	@ResponseBody
+	public String delete(long id) {
+		try {
+			Worker worker = new Worker(id);
+			workerDao.delete(worker);
+		} catch (Exception ex) {
+			return "Error deleting the worker:" + ex.toString();
+		}
+		return "Worker succesfully deleted!";
+	}
 
-    /**
-     * GET /get-by-email  --> Return the id for the user having the passed
-     * email.
-     */
-    @RequestMapping("/get-by-email/{email}")
-    @ResponseBody
-    public String getByEmail(@PathVariable("email") String email) {
-        String userId = "";
-        try {
-            Worker user = workerDao.findByEmail(email);
-            userId = String.valueOf(user.getId());
-        }
-        catch (Exception ex) {
-            return "User not found";
-        }
-        return "The user id is: " + userId;
-    }
-    /**
-     * GET /get-by-name  --> Return the id for the user having the passed
-     * name.
-     */
-    @RequestMapping("/get-by-name/{name}")
-    @ResponseBody
-    public String findByName(@PathVariable("name") String name) {
-        String userId = "";
-        try {
-            Worker user = workerDao.findByName(name);
-            userId = String.valueOf(user.getId());
-        }
-        catch (Exception ex) {
-            return "User not found";
-        }
-        return "The user id is: " + userId;
-    }
-    /**
-     * GET /get-by-job  --> Return the id for the user having the passed
-     * email.
-     */
-    @RequestMapping("/get-by-job/{job}")
-    @ResponseBody
-    public String findByJob(@PathVariable("job") String job) {
-        String userId = "";
-        try {
-            Worker user = workerDao.findByJob(job);
-            userId = String.valueOf(user.getId());
-        }
-        catch (Exception ex) {
-            return "User not found";
-        }
-        return "The user id is: " + userId;
-    }
-    /**
-     * GET /update  --> Update the email and the name for the user in the
-     * database having the passed id.
-     */
-    @RequestMapping("/update")
-    @ResponseBody
-    public String updateUser(long id, String email, String name) {
-        try {
-            Worker worker = workerDao.findById(id).get();
-            worker.setEmail(email);
-            worker.setName(name);
-            workerDao.save(worker);
-        }
-        catch (Exception ex) {
-            return "Error updating the user: " + ex.toString();
-        }
-        return "User succesfully updated!";
-    }
+	/**
+	 * GET /get-by-email --> Return the id for the worker having the passed email.
+	 */
+	@RequestMapping("/get-by-email/{email}")
+	@ResponseBody
+	public String getByEmail(@PathVariable("email") String email) {
+		String workerId = "";
+		try {
+			Worker worker = workerDao.findByEmail(email);
+			workerId = String.valueOf(worker.getId());
+		} catch (Exception ex) {
+			return "Worker not found";
+		}
+		return "The worker id is: " + workerId;
+	}
 
-    // Private fields
+	/**
+	 * GET /get-by-name --> Return the id for the worker having the passed name.
+	 */
+	@RequestMapping("/get-by-name/{name}")
+	@ResponseBody
+	public String findByName(@PathVariable("name") String name) {
+		String workerId = "";
+		try {
+			Worker worker = workerDao.findByName(name);
+			workerId = String.valueOf(worker.getId());
+		} catch (Exception ex) {
+			return "Worker not found";
+		}
+		return "The worker id is: " + workerId;
+	}
 
-    @Autowired
-    private WorkerDao workerDao;
+	/**
+	 * GET /get-by-job --> Return the id for the worker having the passed job.
+	 */
+	@RequestMapping("/get-by-job/{job}")
+	@ResponseBody
+	public String findByJob(@PathVariable("job") String job) {
+		String workerId = "";
+		try {
+			Worker worker = workerDao.findByJob(job);
+			workerId = String.valueOf(worker.getId());
+		} catch (Exception ex) {
+			return "Worker not found";
+		}
+		return "The worker id is: " + workerId;
+	}
+
+	/**
+	 * GET /update --> Update the email and the name for the user in the database
+	 * having the passed id.
+	 */
+	@RequestMapping("/update")
+	@ResponseBody
+	public String updateWorker(long id, String email, String name, String password, String job,
+			List<Appointment> appointments) {
+		try {
+			Worker worker = workerDao.findById(id).get();
+			worker.setEmail(email);
+			worker.setName(name);
+			worker.setPassword(password);
+			worker.setJob(job);
+			worker.setAppointments(appointments);
+			workerDao.save(worker);
+		} catch (Exception ex) {
+			return "Error updating the worker: " + ex.toString();
+		}
+		return "Worker succesfully updated!";
+	}
+
+	// Private fields
+
+	@Autowired
+	private WorkerDao workerDao;
 
 }
